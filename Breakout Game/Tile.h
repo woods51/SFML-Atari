aed1dbd739f72da56b273701604c18c6452e702c
woods51 <65686446+woods51@users.noreply.github.com>
2022-04-07 15:39:20 -0400

    Reworking Tile Class, in progress.

--- Breakout Game/Tile.h
+++ Breakout Game/Tile.h
@@ -23,6 +23,16 @@ public:
 		m_destructable = a_isDestructable;
 		setActive();
 	};
+	Tile(ResourceManager& rm, sf::Vector2f pos = sf::Vector2f(0, 0),
+		TileType type = TileType::Default, std::string textureKey = "0")
+	{
+		m_shape.setPosition(pos);
+		m_shape.setSize(sf::Vector2f(16, 16));
+		m_shape.setScale(sf::Vector2f(2.5f, 2.5f));
+
+		setTile(rm, type, textureKey);
+		setActive();
+	};
 
 	sf::Vector2f getDiagonalPosition() const;
 	inline sf::Vector2f getPosition() const { return m_shape.getPosition(); }
@@ -35,7 +45,37 @@ public:
 	void setActive() { m_active = true; }
 	void setDeactive() { m_active = false; }
 
+	void setTile(ResourceManager& a_rm, TileType a_type = TileType::Default, std::string a_textureKey = "0")
+	{
+		m_type = a_type;
+		m_durability = 1;
+		m_destructable = true;
+
+		switch (a_type)
+		{
+		case TileType::Default:
+			m_defaultTexture = a_textureKey;
+			break;
+		case TileType::Wall:
+			m_defaultTexture = "tile_06";
+			m_destructable = false;
+			break;
+		case TileType::Strong:
+			m_durability = 2;
+			m_defaultTexture = "tile_07";
+			m_secondTexture = a_rm.getTexture("tile_07_1");
+			break;
+		case TileType::Box:
+			m_defaultTexture = "crate";
+			break;
+		default:
+			break;
+		}
+		m_shape.setTexture(a_rm.getTexture(m_defaultTexture));
+	}
+
 protected:
+	TileType m_type;
 	sf::RectangleShape m_shape;
 	int m_durability = 1;
 	bool m_destructable;
@@ -73,4 +113,11 @@ enum class Surface
 	Right,
 	Corner,
 	None
+};
+enum class TileType
+{
+	Default,
+	Strong,
+	Box,
+	Wall
 };
